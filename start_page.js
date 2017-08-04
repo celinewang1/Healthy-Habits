8f58d3cc982fdf8f0359f96d80a6d921c6fb8218
celinewang <celinewanger@gmail.com>
2017-08-04 13:49:44 -0700

    new updates

--- start_page.js
+++ start_page.js
@@ -1,11 +1,13 @@
 function done(){
     var waterAmount = document.getElementById("waterInput").value;
+    /*console.log(waterAmount);*/
     var exerciseAmount = document.getElementById("exerciseInput").value;
+    /*console.log(exerciseAmount);*/
     var waterList = [];
     var exerciseList = [];
     waterList.push(waterAmount);
     exerciseList.push(exerciseAmount);
     console.log(waterList);
     console.log(exerciseList);
-    window.location.href = "tab_page.html";
+    /*window.location.href = "tab_page.html";*/
 }
