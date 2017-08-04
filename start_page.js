596d7540b514a59e41c0e1ce224705ce3998c8a9
courtneyc841 <courtneytchen@gmail.com>
2017-08-04 10:30:00 -0700

    fixed it

--- start_page.js
+++ start_page.js
@@ -1,11 +1,11 @@
 function done(){
-    var waterAmount = document.getElementById("waterInput").innerHTML;
-    var exerciseAmount = document.getElementById("exerciseInput").innerHTML;
-    var waterList = []
-    var exerciseList = []
-    waterList.push(waterAmount)
-    exerciseList.push(exerciseAmount)
-    console.log(waterList)
-    console.log(exerciseList)
-    window.location.href = "tab_page.html"
+    var waterAmount = document.getElementById("waterInput").value;
+    var exerciseAmount = document.getElementById("exerciseInput").value;
+    var waterList = [];
+    var exerciseList = [];
+    waterList.push(waterAmount);
+    exerciseList.push(exerciseAmount);
+    console.log(waterList);
+    console.log(exerciseList);
+    window.location.href = "tab_page.html";
 }
