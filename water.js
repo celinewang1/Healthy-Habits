b0c633e5919099af5d2cc1d7042ea8e907d548d8
shanaya46 <shanayajain2001@gmail.com>
2017-08-08 11:07:36 -0700

    Updated Water and Excercise Page but not fully done

--- water.js
+++ water.js
@@ -1,50 +1,54 @@
 
-var cups = 19;
+var cups = 8;
 var cols = 4;
 var rows = Math.ceil(cups/4);
 if ((cups % 4) != 0){
   var extra = cups % 4;
 }
+var i = 1;
 var table = '<table align = "center"; width = "50%">';
 for(var r = 1; r < (rows+1); r++)
 {
    table += '<tr>';
    if ((r != rows) && (cups >= 4)) {
    for (var c = 0; c < cols; c++) {
-       table += '<td><img id="bottle1" src="empty.png" style="width:100px"/>';
+       var id = "bottle";
+       id += i;
+       table += '<td><img id=id src="empty.png" style="width:100px"/>';
        table += '<br><br>';
-       table += '<button class="button" onclick="document.getElementById(\'bottle1\').src=\'empty.png\'">Empty</button>';
+       table += '<button class="button" onclick="document.getElementById(id).src=\'empty.png\'">Empty</button>';
        table += '<br><br>';
-       table += '<button class="button" onclick="document.getElementById(\'bottle1\').src=\'half.png\'">Fill 4 oz</button>';
+       table += '<button class="button" onclick="document.getElementById(id)\'bottle${i}\'.src=\'half.png\'">Fill 4 oz</button>';
        table += '<br><br>';
-       table += '<button class="button" onclick="document.getElementById(\'bottle1\').src=\'full.png\'">Fill 8 oz</button>';
+       table += '<button class="button" onclick="document.getElementById(id).src=\'full.png\'">Fill 8 oz</button>';
        table += '</td>';
-
+       i = i ++;
      }
    }
    if ((r == rows) && ((cups % 4) !== 0) && (cups >= 4)) {
      for (var c = 0; c < extra; c++){
-       table += '<td><img id="bottle1" src="empty.png" style="width:100px"/>';
+       table += '<td><img id=id="bottle{i}" src="empty.png" style="width:100px"/>';
        table += '<br><br>';
-       table += '<button class="button" onclick="document.getElementById(\'bottle1\').src=\'empty.png\'">Empty</button>';
+       table += '<button class="button" onclick="document.getElementById(\'bottle{i}\').src=\'empty.png\'">Empty</button>';
        table += '<br><br>';
-       table += '<button class="button" onclick="document.getElementById(\'bottle1\').src=\'half.png\'">Fill 4 oz</button>';
+       table += '<button class="button" onclick="document.getElementById(\'bottle{i}\').src=\'half.png\'">Fill 4 oz</button>';
        table += '<br><br>';
-       table += '<button class="button" onclick="document.getElementById(\'bottle1\').src=\'full.png\'">Fill 8 oz</button>';
+       table += '<button class="button" onclick="document.getElementById(\'bottle{i}\').src=\'full.png\'">Fill 8 oz</button>';
        table += '</td>';
+       i = i++;
      }
    }
    if ((cups < 4) && (r != rows)){
      for (var c = 0; c < cups; c++) {
-         table += '<td><img id="bottle1" src="empty.png" style="width:100px"/>';
+         table += '<td><img id=id="bottle{i}" src="empty.png" style="width:100px"/>';
          table += '<br><br>';
-         table += '<button class="button" onclick="document.getElementById(\'bottle1\').src=\'empty.png\'">Empty</button>';
+         table += '<button class="button" onclick="document.getElementById(\'bottle{i}\').src=\'empty.png\'">Empty</button>';
          table += '<br><br>';
-         table += '<button class="button" onclick="document.getElementById(\'bottle1\').src=\'half.png\'">Fill 4 oz</button>';
+         table += '<button class="button" onclick="document.getElementById(\'bottle{i}\').src=\'half.png\'">Fill 4 oz</button>';
          table += '<br><br>';
-         table += '<button class="button" onclick="document.getElementById(\'bottle1\').src=\'full.png\'">Fill 8 oz</button>';
+         table += '<button class="button" onclick="document.getElementById(\'bottle{i}\').src=\'full.png\'">Fill 8 oz</button>';
          table += '</td>';
-
+         i = i++;
        }
    }
    table += '</tr>';
