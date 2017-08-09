840703eba1ca0ca7087c0efdbe33e861a47353c5
celinewang <celinewanger@gmail.com>
2017-08-08 22:00:25 -0700

    testing

--- dumbell.js
+++ dumbell.js
@@ -7,6 +7,7 @@ if ((half % 4) != 0){
   var extra = half % 4;
 }
 var table = '<table style="width:75%" align="center">';
+
 for(var r = 1; r < (rows+1); r++)
 {
    table += '<tr>';
@@ -55,5 +56,19 @@ for(var r = 1; r < (rows+1); r++)
 table += '</table>';
 console.log(table);
 
-
 document.write(table);
+
+
+var waterAmount = document.getElementById("waterInput").value;
+/*console.log(waterAmount);*/
+var exerciseAmount = document.getElementById("exerciseInput").value;
+/*console.log(exerciseAmount);*/
+var waterList = [];
+var exerciseList = [];
+waterList.push(waterAmount);
+exerciseList.push(exerciseAmount);
+console.log(waterList);
+console.log(exerciseList);
+url="https" + "://celinewang1.github.io/Healthy-Habits/tab_page.html?wid=" + encodeURIComponent(waterAmount) + "eid=" + encodeURIComponent(exerciseAmount);
+//url="file"+":///C:/Users/Admin/Healthy-Habits/tab_page.html?wid=" + encodeURIComponent(waterAmount) + "eid=" + encodeURIComponent(exerciseAmount);
+window.location.href = url;
