771c43fd543df5fdaaa7843a706f42eae38b978f
celinewang <celinewanger@gmail.com>
2017-08-10 09:19:36 -0700

    new

--- start_page.js
+++ start_page.js
@@ -2,7 +2,6 @@ function done(){
     var waterAmount = document.getElementById("waterInput").value;
     /*console.log(waterAmount);*/
     var exerciseAmount = document.getElementById("exerciseInput").value;
-    localStorage.setItem('waterCups', waterAmount);
     /*console.log(exerciseAmount);*/
     var waterList = [];
     var exerciseList = [];
@@ -12,5 +11,5 @@ function done(){
     console.log(exerciseList);
     url="https" + "://celinewang1.github.io/Healthy-Habits/tab_page.html?wid=" + encodeURIComponent(waterAmount) + "eid=" + encodeURIComponent(exerciseAmount);
     window.location.href = url;
-    
+
 }
